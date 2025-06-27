03e710fe62e0ff41eaf353aff863986b92ec38ce
ViruG953 <gaikwadviren3@gmail.com>
2025-06-27 20:42:10 +0530

    Create Function_1.php
    The first function file for adding 2 numbers!!!!!

---
+++ Function_1.php
@@ -0,0 +1,9 @@
+//Write a PHP code use a function to add 2 numbers
+
+<?php
+function add($num1,$num2){
+$res=$num1+$num2;
+echo"THE ADDITION OF".$num1."AND".$num2."=".$res."<br>";
+}
+add(5,5);
+?>
